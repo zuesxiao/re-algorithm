94605c42bd8157386a448ed2e6d489d3d8806af8
Xiao <jeff.xiao@sap.com>
2020-12-27 11:05:18 +0800

    add solution for question 103

---
+++ src/com/xiaoling/leetcode/solution/Q103.java
@@ -0,0 +1,52 @@
+package com.xiaoling.leetcode.solution;
+
+import com.xiaoling.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author xialing
+ */
+public class Q103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        boolean leftToRight = true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int currentSize = queue.size();
+            List<Integer> temp = new LinkedList<>();
+            for (int i = 0; i < currentSize; i++) {
+                TreeNode node = queue.poll();
+
+                if (node == null) {
+                    continue;
+                }
+
+                if (leftToRight) {
+                    temp.add(node.val);
+                } else {
+                    temp.add(0, node.val);
+                }
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            leftToRight = !leftToRight;
+            result.add(temp);
+        }
+        return result;
+    }
+}
